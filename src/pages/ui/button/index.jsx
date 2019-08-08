497dffefc06bb2fb6e32bd4a9f9c911c36eb2f5e
unknown <backlight.wu>
2019-08-08 18:04:48 +0800

    "button组件"

--- src/pages/ui/button/index.jsx
+++ src/pages/ui/button/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Card } from 'antd'
+import { Button, Card, Radio } from 'antd'
 
 import './index.less'
 
@@ -7,13 +7,20 @@ export default class Buttons extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loagdingFlag: true
+            loagdingFlag: true,
+            size:"default"
         }
     }
 
     handIsLoading = () => {
         this.setState({
             loagdingFlag: !this.state.loagdingFlag
+        })
+    }
+
+    handChangeRadio = (e) => {
+        this.setState({
+            size: e.target.value
         })
     }
 
@@ -41,6 +48,25 @@ export default class Buttons extends React.Component {
                     <Button type="primary" shape='circle' loading={this.state.loagdingFlag}></Button>
                     <Button loading={this.state.loagdingFlag}>点击加载</Button>
                     <Button type="primary" onClick={this.handIsLoading}>加载 </Button>
+                </Card>
+                <Card title="按钮组">
+                    <Button.Group>
+                        <Button type="primary" icon="left" style={{margin:"0px"}}>返回</Button>
+                        <Button type="primary" icon="right">前进</Button>
+                    </Button.Group>
+                </Card>
+                <Card title="按钮尺寸">
+                    <Radio.Group value={this.state.size} onChange={this.handChangeRadio}>
+                        <Radio value="small">小</Radio>
+                        <Radio value="default">中</Radio>
+                        <Radio value="large">大</Radio>
+                    </Radio.Group>
+                    <Button type="primary" size={this.state.size}>主按钮</Button>
+                    <Button size={this.state.size}>普通按钮</Button>
+                    <Button type="dashed" size={this.state.size}>虚线按钮</Button>
+                    <Button type="danger" size={this.state.size}>删除按钮</Button>
+                    <Button disabled size={this.state.size}>不可选按钮</Button>
+                    <Button type="link" size={this.state.size}>链接按钮</Button>
                 </Card>
             </div>
         )
